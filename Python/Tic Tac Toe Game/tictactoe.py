ffc0f4cb35992bef62d31859d68dea431bc01b75
Yann Kouadio <kouadioacka@gmail.com>
2020-02-01 14:28:14 +0100

    Update tictactoe.py

--- Python/Tic Tac Toe Game/tictactoe.py
+++ Python/Tic Tac Toe Game/tictactoe.py
@@ -13,49 +13,49 @@ def game_init(players_dict, player_number=2):
     # Let the player choose their symbol
     for i in range(player_number):
         while True:
-            nameInput = input(
+            name_input = input(
                 f"\t{players_dict[i]['name']}, please enter your name (default = {players_dict[i]['name']}) : ")
 
             if i == 0:
-                if len(nameInput) > 0 and nameInput.capitalize() != players_dict[i + 1]['name']:
-                    players_dict[i]['name'] = nameInput.capitalize()
+                if len(name_input) > 0 and name_input.capitalize() != players_dict[i + 1]['name']:
+                    players_dict[i]['name'] = name_input.capitalize()
                     break
-                elif len(nameInput) == 0:
+                if len(name_input) == 0:
                     break
 
             else:
-                if len(nameInput) > 0 and nameInput.capitalize() != players_dict[i - 1]['name']:
-                    players_dict[i]['name'] = nameInput.capitalize()
+                if len(name_input) > 0 and name_input.capitalize() != players_dict[i - 1]['name']:
+                    players_dict[i]['name'] = name_input.capitalize()
                     break
-                elif len(nameInput) == 0:
+                if len(name_input) == 0:
                     break
 
         while True:
-            symbolInput = input(
+            symbol_input = input(
                 f"\t{players_dict[i]['name']}, please choose your symbol (default = {players_dict[i]['symbol']}) : ")
 
             if i == 0:
-                if len(symbolInput) > 0 and symbolInput != players_dict[i + 1]['symbol']:
-                    players_dict[i]['symbol'] = symbolInput.upper()
+                if len(symbol_input) > 0 and symbol_input.upper() != players_dict[i + 1]['symbol']:
+                    players_dict[i]['symbol'] = symbol_input.upper()
                     break
-                elif len(symbolInput) == 0:
+                if len(symbol_input) == 0:
                     break
 
             else:
-                if len(symbolInput) > 0 and symbolInput != players_dict[i - 1]['symbol']:
-                    players_dict[i]['symbol'] = symbolInput.upper()
+                if len(symbol_input) > 0 and symbol_input.upper() != players_dict[i - 1]['symbol']:
+                    players_dict[i]['symbol'] = symbol_input.upper()
                     break
-                elif len(symbolInput) == 0:
+                if len(symbol_input) == 0:
                     break
 
         print(f"\t\t- Your symbol is : {players_dict.get(i)['symbol']}")
         print('')
 
     # Ask to the player if their are ready to start
-    readyInput = input("\tAre you ready to play? Enter Yes or No")
+    ready_input = input("\tAre you ready to play? Enter Yes or No")
 
-    while readyInput.lower() not in ['yes', 'y', 'ye', '']:
-        readyInput = input("\tAre you ready to play? Enter Yes or No")
+    while ready_input.lower() not in ['yes', 'y', 'ye', '']:
+        ready_input = input("\tAre you ready to play? Enter Yes or No")
 
     return players_dict, True
 
@@ -98,16 +98,16 @@ def has_winner(data_frame, iterator, player_symbol):
 
     if iterator == 'row':
         for i in range(0, len(keys)):
-            rowVal = list(set(data_frame.iloc[i]))
+            row_val = list(set(data_frame.iloc[i]))
 
-            if len(rowVal) == 1 and rowVal[0] == player_symbol:
+            if len(row_val) == 1 and row_val[0] == player_symbol:
                 result = True
 
     if iterator == 'col':
         for key in keys:
-            colVal = list(set(data_frame.iloc[:][key]))
+            col_val = list(set(data_frame.iloc[:][key]))
 
-            if len(colVal) == 1 and colVal[0] == player_symbol:
+            if len(col_val) == 1 and col_val[0] == player_symbol:
                 result = True
 
     if iterator == 'diag':
@@ -125,22 +125,25 @@ def has_winner(data_frame, iterator, player_symbol):
 
 
 def check_winner(board_list, player_symbol):
+    """
+        Create data frame from the list, and loop through each col, row and diag
+    """
     result = False
 
     # Create a dataframe from the board list
-    boardDict = {'col1': list(), 'col2': list(), 'col3': list()}
+    board_dict = {'col1': list(), 'col2': list(), 'col3': list()}
 
     for i in range(0, len(board_list) - 1, 3):
-        boardDict['col1'].append(board_list[i + 1])
-        boardDict['col2'].append(board_list[i + 2])
-        boardDict['col3'].append(board_list[i + 3])
+        board_dict['col1'].append(board_list[i + 1])
+        board_dict['col2'].append(board_list[i + 2])
+        board_dict['col3'].append(board_list[i + 3])
 
-    boardDataFrame = pd.DataFrame(boardDict)
+    board_dataframe = pd.DataFrame(board_dict)
 
     iterators = ['row', 'col', 'diag']
 
     for item in iterators:
-        result = has_winner(data_frame=boardDataFrame, iterator=item, player_symbol=player_symbol)
+        result = has_winner(data_frame=board_dataframe, iterator=item, player_symbol=player_symbol)
 
         if result:
             break
@@ -149,6 +152,10 @@ def check_winner(board_list, player_symbol):
 
 
 def play_game(board_list, players_dict):
+    """
+        Main game function
+    """
+
     print("\n\tGame starts ...")
     print('')
 
@@ -157,44 +164,44 @@ def play_game(board_list, players_dict):
     print('')
 
     # Variables initialization
-    activePlayer = 0
-    gameEnd = False
+    active_player = 0
+    game_end = False
     i = 0
 
-    while i < len(board_list) - 1 and not gameEnd:
-        inputValue = input(f"\t {players_dict[activePlayer]['name']} please choose your position (1-9) : ")
+    while i < len(board_list) - 1 and not game_end:
+        input_value = input(f"\t {players_dict[active_player]['name']} please choose your position (1-9) : ")
 
         # Verify that the input is not a space and is a number between 1 and 9
         # Verify that also it has not already been inserted
-        incorrectInput = True
-        while incorrectInput:
-            if inputValue in '123456789' and len(inputValue) == 1:
-                if len(board_list[int(inputValue)]) == 0:
-                    incorrectInput = False
+        incorrect_input = True
+        while incorrect_input:
+            if input_value in '123456789' and len(input_value) == 1:
+                if len(board_list[int(input_value)]) == 0:
+                    incorrect_input = False
                 else:
                     print("\t\t\t - Sorry, movement not allow")
 
-            if incorrectInput:
-                inputValue = input("\t\t - Please choose a value between 1-9 : ")
+            if incorrect_input:
+                input_value = input("\t\t - Please choose a value between 1-9 : ")
 
         # Add the update the value in the boardList and display the updated board
-        playerSymbol = players_dict[activePlayer]['symbol']
-        board_list[int(inputValue)] = playerSymbol
+        player_symbol = players_dict[active_player]['symbol']
+        board_list[int(input_value)] = player_symbol
         print('')
         display_board(board=board_list)
 
         # Verify if we have a winner
-        gameEnd = check_winner(board_list=board_list, player_symbol=playerSymbol)
+        game_end = check_winner(board_list=board_list, player_symbol=player_symbol)
 
         # Change the active player
-        if not gameEnd:
-            activePlayer = 1 if activePlayer == 0 else 0
+        if not game_end:
+            active_player = 1 if active_player == 0 else 0
 
         i += 1
         print('')
 
-    if gameEnd:
-        print(f"{players_dict[activePlayer]['name']} won the game")
+    if game_end:
+        print(f"{players_dict[active_player]['name']} won the game")
     else:
         print('\tWe have a draw')
 
@@ -202,33 +209,33 @@ def play_game(board_list, players_dict):
 
 
 def start_game():
+    """
+        Start and initialize the games variables
+    """
+
     print('\t****************************************************')
     print(f"\t {' ' * 10} Welcome to Tic Tac Toe!")
     print('\tPlease consider your numpad as the grid for the game')
     print('\t****************************************************')
     print('')
 
-    """
-        variables initialization
-    """
-    playersDict = {0: {'name': 'Player 1', 'symbol': 'X'},
-                   1: {'name': 'Player 2', 'symbol': 'O'}}
-    boardList = [''] * 10
+    # variables initialization
+    players_dict = {0: {'name': 'Player 1', 'symbol': 'X'},
+                    1: {'name': 'Player 2', 'symbol': 'O'}}
+    board_list = [''] * 10
 
-    """
-       game initializations
-    """
-    playersDict, gameHasStarted = game_init(players_dict=playersDict)
+    # game initializations
+    players_dict, game_has_started = game_init(players_dict=players_dict)
 
-    while gameHasStarted:
-        play_game(board_list=boardList, players_dict=playersDict)
+    while game_has_started:
+        play_game(board_list=board_list, players_dict=players_dict)
 
         replay = input("Do you want to play again (Yes or No) ? ")
 
-        boardList = [''] * 10
+        board_list = [''] * 10
 
         if replay.lower() not in ['yes', 'ye', 'y']:
-            gameHasStarted = False
+            game_has_started = False
             print("\nThank for your time, Bye ;-)")
 
 
